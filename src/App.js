c5dc5b25610afad451295afb50c4d5639462823c
Tim <mitelmant@yahoo.com>
2023-06-22 10:25:27 -0400

    run games initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='card-shape'>
+      <img className='card-image' src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/d823e0070b5f6e85f55bb68739e879f0.jpg"></img>
+      <div className='card-text'>
+        <div className='title'>Diablo 4</div>
+        <div className='info'>BLah Blah Blah</div>
+        <div className='launch'>Launch</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
