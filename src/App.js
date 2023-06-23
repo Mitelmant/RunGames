f22c6c113d8606fc9e15cc532847fcfbec497de3
spycewyrm <ytx0a58kxcog@opayq.com>
2023-06-23 14:53:12 -0400

    Card now moves and spins when clicked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 
 function Tilt(props) {
@@ -12,7 +12,10 @@ function Tilt(props) {
   return <div ref={tilt} {...rest} />
 }
 
+
 function App() {
+  const [flyoutToggled, toggleFlyout] = useState(false);
+
   const options = {
     max: 10,
     speed: 300,
@@ -21,11 +24,11 @@ function App() {
   }
 
   return (
-    <Tilt className="card-shape" options={options}>
+    <Tilt className={`${flyoutToggled ? "card-shape flyout" : "card-shape"}`} onClick={() => toggleFlyout(true)} options={options}>
       <img className="card-image" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/137bbbfdf66395b40d543a9ec56d3c8a.jpg"></img>
       <div className="card-text">
         <div className="title">Solasta: Crown of the Magister</div>
-        <div className="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in nibh mauris cursus. Nulla facilisi cras fermentum odio eu. Diam quam nulla porttitor massa. Est velit egestas dui id ornare arcu odio ut sem.</div>
+        <div className="info">Roll for initiative, take attacks of opportunity, manage player location and the verticality of the battle field in this Turn-Based Tactical RPG based on the SRD 5.1 Ruleset. In Solasta, you make the choices, dice decide your destiny.</div>
         <a className="launch" href="https://youtu.be/p7YXXieghto" target="_blank">Launch</a>
       </div>
     </Tilt>
